027b0266dba38c8b3fb9a035887ad7a0500b0287
Cifer-dev <82645865+Cifer-dev@users.noreply.github.com>
2021-05-27 21:49:22 +0100

    Game Finished
    everything more or less done
    there is an constant error but the game still works
    cant make projectiles shoot different direction and spin

---
+++ Dungeon Crawler - Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public variable, visible in unity inspector
+    public float forceStrength;
+    
+    //public functions to be called by each of the buttons
+    public void MoveUp()
+    {
+        //get the rigibody that well be ysing fro the movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //move in the correc direction with the set force strength
+        ourRigidbody.AddForce(Vector2.up * forceStrength);
+    }
+    public void MoveLeft()
+    {
+        //get the rigibody that well be ysing fro the movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //move in the correc direction with the set force strength
+        ourRigidbody.AddForce(Vector2.left * forceStrength);
+    }
+    public void MoveDown()
+    {
+        //get the rigibody that well be ysing fro the movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //move in the correc direction with the set force strength
+        ourRigidbody.AddForce(Vector2.down * forceStrength);
+    }
+    public void MoveRight()
+    {
+        //get the rigibody that well be ysing fro the movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //move in the correc direction with the set force strength
+        ourRigidbody.AddForce(Vector2.right * forceStrength);
+    }
+}
---
+++ Dungeon Crawler - Unity Project/Assets/Scripts/ScoreKill.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKill : MonoBehaviour
+{
+    // Public variables
+    // Editable in Unity. accessable by other scripts
+    public int killValue = 1;
+}
